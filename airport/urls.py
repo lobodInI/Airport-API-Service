53a33804044779f714b7deb2d61fc3f2ee5f3ab7
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-08 17:59:48 +0200

    created urls.py for app airport

---
+++ airport/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    LocationViewSet,
+    AirportViewSet,
+    CrewViewSet,
+    RouteViewSet,
+    FlightViewSet
+)
+
+router = routers.DefaultRouter()
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("locations", LocationViewSet)
+router.register("airports", AirportViewSet)
+router.register("crews", CrewViewSet)
+router.register("routes", RouteViewSet)
+router.register("flights", FlightViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "airport"
