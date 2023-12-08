dd0dac899f4592b66b8a73e4be2e20d77cb8cd3e
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-08 17:58:34 +0200

    created views for Model: Order, Ticket

--- order/views.py
+++ order/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from order.models import Order
+from order.serializers import OrderSerializer, OrderListSerializer
+
+
+class OrderPagination(PageNumberPagination):
+    page_size = 5
+    max_page_size = 80
+
+
+class OrderViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.prefetch_related(
+        "tickets__flight__route", "tickets__flight__airplane"
+    )
+    serializer_class = OrderSerializer
+    pagination_class = OrderPagination
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderListSerializer
+
+        return OrderSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
