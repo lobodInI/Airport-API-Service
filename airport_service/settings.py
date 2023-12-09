57079752590d49346eb590a995438945cd7f9033
serhii_loboda <serhiy.loboda@gmail.com>
2023-12-09 17:09:18 +0200

    delete template folder

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -67,7 +67,7 @@ ROOT_URLCONF = 'airport_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': []
         ,
         'APP_DIRS': True,
         'OPTIONS': {
